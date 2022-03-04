d3ad1bba1418ec2c6f3228ef0b5973599fe5dbdd
Nithyadevi <nithi2218@gmail.com>
2022-03-04 12:37:03 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'Nithi S';
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+const shortGreeting = 'Welcome, ' + firstInitial + '!';
+
+
+
+
